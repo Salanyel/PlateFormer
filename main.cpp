ec1f79eea3459202cff96509bcba28fa20191d66
Xyunn <maxime.devecchi@gmail.com>
2014-12-17 10:12:02 +0100

    Begin of the Finite State Machine.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "StateMachine_Game.h"
+
+using namespace std;
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(200, 200), "PlateFormer");
+	
+	StateMachine_Game gameMachine(&window);
+	cout << "GameMachine created" << endl;
+	if (gameMachine.changeState(GAME_STATES::MENU))
+	{
+		cout << "State initalized" << endl;
+	} else {
+		cout << "Error during the state change init" << endl;
+	}
+
+	Event event;
 
 	while (window.isOpen())
 	{
-		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
 		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
 	}
-
-	return 0;
 }
